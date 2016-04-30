816052fa661aa2e3177d0b1866b783a3ef33660b
eloyedm <eloy.edm@gmail.com>
2016-04-30 09:28:53 -0500

    empresas con combo funcional

--- Prueba proyecto bdat/frm_CatalogoDeEmpresas.vb
+++ Prueba proyecto bdat/frm_CatalogoDeEmpresas.vb
@@ -3,6 +3,15 @@
     Dim puedoAgregar = False
     Private Sub LoadDataGrid()
         Me.dgrid_Empresa.DataSource = ConnectionModule.connection.ReaderCommand("SELECT idEmpresa, razonSocial, nombreComercial, RFC FROM Empresa WHERE activo = 1", "Empresa").Tables("Empresa")
+        Me.dgrid_Empresa.Columns("razonSocial").DisplayIndex = 0
+        Me.dgrid_Empresa.Columns("nombreComercial").DisplayIndex = 1
+        Me.dgrid_Empresa.Columns("RFC").DisplayIndex = 2
+    End Sub
+
+    Private Sub frm_CatalogoDeEmpresas_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Me.Hide()
+        frm_Menu.Show()
+        frm_Menu.Focus()
     End Sub
 
     Private Sub ResetControls()
@@ -13,27 +22,42 @@
         dtpInicioDeOperaciones.Text = ""
         txtCalle.Text = ""
         txtColonia.Text = ""
-        cbEstado.Text = "Seleccione..."
-        cbMunicipio.Text = "Seleccione..."
+        cbEstado.Text = ""
+        cbMunicipio.Text = ""
+        txtCp.Text = ""
 
     End Sub
+
+    Private Sub LoadComboBox()
+
+        Dim Estados As DataTable = ConnectionModule.connection.ReaderCommand("SELECT idEstado, nombreEstado FROM Estado", "Estado").Tables("Estado")
+        cbEstado.Items.Add("-SELECCIONA-")
+        cbMunicipio.Items.Add("-SELECCIONA-")
+        For index As Integer = 0 To Estados.Rows.Count() - 1
+            cbEstado.Items.Add(Estados.Rows(index).Item("nombreEstado"))
+        Next
+        cbEstado.SelectedIndex = 0
+        cbMunicipio.SelectedIndex = 0
+    End Sub
+
 
     Private Sub frm_CatalogoDeEmpresas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadComboBox()
         LoadDataGrid()
-        cbEstado.DataSource = ConnectionModule.connection.ReaderCommand("SELECT idEstado, nombreEstado FROM Estado", "Estado").Tables("Estado")
-        cbEstado.DisplayMember = "nombreEstado"
-        cbEstado.ValueMember = "idEstado"
-
     End Sub
+
+   
+
+  
 
     Private Sub cbEstado_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbEstado.SelectedIndexChanged
-        Dim seleccion = cbMunicipio.SelectedIndex
-        cbMunicipio.DataSource = ConnectionModule.connection.ReaderCommand("SELECT idCiudad, nombreCiudad FROM Ciudad WHERE idEstado = " & cbEstado.SelectedIndex & "", "Ciudad").Tables("Ciudad")
-        cbMunicipio.DisplayMember = "nombreCiudad"
-        cbMunicipio.ValueMember = "idCiudad"
+        Dim Ciudades As DataTable = ConnectionModule.connection.ReaderCommand("SELECT idCiudad, nombreCiudad FROM Ciudad WHERE idEstado = " & cbEstado.SelectedIndex & "", "Ciudad").Tables("Ciudad")
+        For index As Integer = 0 To Ciudades.Rows.Count() - 1
+            cbMunicipio.Items.Add(Ciudades.Rows(index).Item("nombreCiudad"))
+        Next
     End Sub
 
-
+   
     Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
         If txtRazonSocial.Text <> "" And txtRFC.Text <> "" And txtNombre.Text <> "" And txtCalle.Text <> "" And
             txtNumero.Text <> "" And txtColonia.Text <> "" And cbEstado.Text <> "" And cbMunicipio.Text <> "" And
@@ -62,11 +86,5 @@
             txtRFC.Focus()
         End If
 
-    End Sub
-
-    Private Sub frm_CatalogoDeEmpresas_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
-        Me.Hide()
-        frm_Menu.Show()
-        frm_Menu.Focus()
     End Sub
 End Class
