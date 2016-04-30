8664d2b23354188dfa85228eed23f505820877cd
Luis Adrian Almaguer de la Garza <imaximuz@gmail.com>
2016-04-29 21:32:00 -0500

    Agregadas las funciones de agregar y modificar

--- Prueba proyecto bdat/frm_CatalogoDeEmpleado.vb
+++ Prueba proyecto bdat/frm_CatalogoDeEmpleado.vb
@@ -10,12 +10,12 @@
 
     Private Sub LoadDataGrid()
         Me.dgrid_Empleado.DataSource = ConnectionModule.connection.ReaderCommand("SELECT idEmpleado, nombreEmpleado, apellidoPaterno, apellidoMaterno, RFC, tarjetaVales FROM Empleado WHERE activo = 1", "Empleado").Tables("Empleado")
-        Me.dgrid_Empleado.Columns("Nombre").DisplayIndex = 0
-        Me.dgrid_Empleado.Columns("Apellido P.").DisplayIndex = 1
-        Me.dgrid_Empleado.Columns("Apellido M.").DisplayIndex = 2
-        Me.dgrid_Empleado.Columns("RFC").DisplayIndex = 3
-        Me.dgrid_Empleado.Columns("Tarjeta").DisplayIndex = 4
-        Me.dgrid_Empleado.Columns("Borrar").DisplayIndex = 5
+        'Me.dgrid_Empleado.Columns("Nombre").DisplayIndex = 0
+        'Me.dgrid_Empleado.Columns("Apellido P.").DisplayIndex = 1
+        'Me.dgrid_Empleado.Columns("Apellido M.").DisplayIndex = 2
+        'Me.dgrid_Empleado.Columns("RFC").DisplayIndex = 3
+        'Me.dgrid_Empleado.Columns("Tarjeta").DisplayIndex = 4
+        'Me.dgrid_Empleado.Columns("Borrar").DisplayIndex = 5
     End Sub
 
     Private Sub ResetControls()
@@ -35,7 +35,47 @@
         LoadDataGrid()
     End Sub
 
-    Private Sub dgrid_Empleado_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgrid_Empleado.CellContentClick
+
+    Private Sub btnAceptar_Click(sender As Object, e As EventArgs) Handles btnAceptar.Click
+
+        If txtNombre.Text <> "" And txtApellidoP.Text <> "" And (txtApellidoM.Text <> "") And txtRFC.Text <> "" Then
+            ' If (txtTelefono.Text.Length >= 8) Then
+            If actualizando <> True Then
+                puedoAgregar = True
+                ' Else
+                '   MessageBox.Show("El telefono debe ser de minimo 8 digitos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                ' End If
+            Else
+                puedoAgregar = True
+            End If
+        Else
+            MessageBox.Show("Todos los campos son obligatorios.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+
+        If puedoAgregar = True Then
+            If txtNumeroEmpleado.Text = "" Then
+                ' Falta obtener el id de la empresa
+                Dim numeroTarjeta = 0
+                ConnectionModule.connection.NonQueryCommand("INSERT INTO Empleado (idEmpresa, nombreEmpleado, apellidoPaterno, apellidoMaterno, RFC, tarjetaVales) VALUES (1,'" & txtNombre.Text & "','" & txtApellidoP.Text & "','" & txtApellidoM.Text & "','" & txtRFC.Text & "', '" & numeroTarjeta & "')")
+            Else
+                ConnectionModule.connection.NonQueryCommand("UPDATE Empleado SET nombreEmpleado = '" + txtNombre.Text + "', apellidoPaterno = '" +
+                                                            txtApellidoP.Text + "', apellidoMaterno = '" + txtApellidoM.Text + "', RFC = '" + txtRFC.Text +
+                                                            "' WHERE '" + txtNumeroEmpleado.Text + "' = idEmpleado")
+            End If
+
+            btnAceptar.Text = "Agregar"
+            actualizando = False
+            puedoAgregar = False
+            btnAceptar.BackColor = Color.FromArgb(0, 192, 0)
+            ConnectionModule.connection.Clear(dgrid_Empleado)
+            ResetControls()
+            LoadDataGrid()
+            txtNombre.Focus()
+        End If
+
+    End Sub
+
+    Private Sub dgrid_Empleado_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgrid_Empleado.CellClick
         actualizando = True
         If e.RowIndex < dgrid_Empleado.RowCount - 1 Then
 
@@ -55,20 +95,20 @@
             If txtNumeroEmpleado.Text <> "" Then
                 Dim response As MsgBoxResult
 
-                response = MsgBox("¿Estás seguro que quieres eliminar al usuario '" & txtNombre.Text & "'?", MsgBoxStyle.YesNo, "Confirmación")
+                response = MsgBox("¿Estás seguro que quieres eliminar al empleado '" & txtNombre.Text & "'?", MsgBoxStyle.YesNo, "Confirmación")
 
                 If response = MsgBoxResult.Yes Then
-                    ConnectionModule.connection.NonQueryCommand("UPDATE Usuario SET activo = 0 WHERE idUsuario = " & txtNumeroEmpleado.Text)
+                    ConnectionModule.connection.NonQueryCommand("UPDATE Empleado SET activo = 0 WHERE idEmpleado = " & txtNumeroEmpleado.Text)
                     ConnectionModule.connection.Clear(dgrid_Empleado)
 
                     ResetControls()
                     LoadDataGrid()
                     btnAceptar.Text = "Agregar"
-                    actualizando = False
                     btnAceptar.BackColor = Color.FromArgb(0, 192, 0)
+                    actualizando = False
                 End If
             End If
         End If
-    End Sub
 
+    End Sub
 End Class
