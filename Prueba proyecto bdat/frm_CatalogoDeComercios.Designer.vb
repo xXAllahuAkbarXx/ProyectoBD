7f0eff4197963f15627e081d398f0cb4851c555e
PanicJoker03 <buddynod32@hotmail.com>
2016-04-29 21:58:56 -0500

    comercios autorizados

--- Prueba proyecto bdat/frm_CatalogoDeComercios.Designer.vb
+++ Prueba proyecto bdat/frm_CatalogoDeComercios.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frm_CatalogoDeComercios
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class frm_CatalogoDeComercios
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.lblRFC = New System.Windows.Forms.Label()
         Me.txtRFC = New System.Windows.Forms.TextBox()
@@ -28,24 +28,20 @@ Partial Class frm_CatalogoDeComercios
         Me.txtRazonSocial = New System.Windows.Forms.TextBox()
         Me.lblNombre = New System.Windows.Forms.Label()
         Me.txtNombre = New System.Windows.Forms.TextBox()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.IDComercio = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.razonSocial = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.nombreComercial = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.rfc = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.dGridComercios = New System.Windows.Forms.DataGridView()
         Me.btnAceptar = New System.Windows.Forms.Button()
         Me.btnCancelar = New System.Windows.Forms.Button()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.txtID = New System.Windows.Forms.TextBox()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dGridComercios, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'lblRFC
         '
         Me.lblRFC.AutoSize = True
-        Me.lblRFC.Location = New System.Drawing.Point(12, 121)
+        Me.lblRFC.Location = New System.Drawing.Point(12, 150)
         Me.lblRFC.Name = "lblRFC"
         Me.lblRFC.Size = New System.Drawing.Size(28, 13)
         Me.lblRFC.TabIndex = 1
@@ -53,7 +49,7 @@ Partial Class frm_CatalogoDeComercios
         '
         'txtRFC
         '
-        Me.txtRFC.Location = New System.Drawing.Point(15, 137)
+        Me.txtRFC.Location = New System.Drawing.Point(15, 166)
         Me.txtRFC.Name = "txtRFC"
         Me.txtRFC.Size = New System.Drawing.Size(201, 20)
         Me.txtRFC.TabIndex = 2
@@ -71,13 +67,13 @@ Partial Class frm_CatalogoDeComercios
         '
         Me.txtRazonSocial.Location = New System.Drawing.Point(15, 88)
         Me.txtRazonSocial.Name = "txtRazonSocial"
-        Me.txtRazonSocial.Size = New System.Drawing.Size(201, 20)
+        Me.txtRazonSocial.Size = New System.Drawing.Size(577, 20)
         Me.txtRazonSocial.TabIndex = 4
         '
         'lblNombre
         '
         Me.lblNombre.AutoSize = True
-        Me.lblNombre.Location = New System.Drawing.Point(232, 72)
+        Me.lblNombre.Location = New System.Drawing.Point(12, 111)
         Me.lblNombre.Name = "lblNombre"
         Me.lblNombre.Size = New System.Drawing.Size(92, 13)
         Me.lblNombre.TabIndex = 5
@@ -85,41 +81,19 @@ Partial Class frm_CatalogoDeComercios
         '
         'txtNombre
         '
-        Me.txtNombre.Location = New System.Drawing.Point(235, 88)
+        Me.txtNombre.Location = New System.Drawing.Point(15, 127)
         Me.txtNombre.Name = "txtNombre"
-        Me.txtNombre.Size = New System.Drawing.Size(198, 20)
+        Me.txtNombre.Size = New System.Drawing.Size(577, 20)
         Me.txtNombre.TabIndex = 6
         '
-        'DataGridView1
+        'dGridComercios
         '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IDComercio, Me.razonSocial, Me.nombreComercial, Me.rfc})
-        Me.DataGridView1.Location = New System.Drawing.Point(15, 196)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(418, 278)
-        Me.DataGridView1.TabIndex = 7
-        '
-        'IDComercio
-        '
-        Me.IDComercio.DataPropertyName = "idComercio"
-        Me.IDComercio.HeaderText = "ID"
-        Me.IDComercio.Name = "IDComercio"
-        Me.IDComercio.Visible = False
-        '
-        'razonSocial
-        '
-        Me.razonSocial.HeaderText = "Razon social"
-        Me.razonSocial.Name = "razonSocial"
-        '
-        'nombreComercial
-        '
-        Me.nombreComercial.HeaderText = "Nombre comercial"
-        Me.nombreComercial.Name = "nombreComercial"
-        '
-        'rfc
-        '
-        Me.rfc.HeaderText = "RFC"
-        Me.rfc.Name = "rfc"
+        Me.dGridComercios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dGridComercios.Location = New System.Drawing.Point(15, 196)
+        Me.dGridComercios.Name = "dGridComercios"
+        Me.dGridComercios.ReadOnly = True
+        Me.dGridComercios.Size = New System.Drawing.Size(577, 289)
+        Me.dGridComercios.TabIndex = 7
         '
         'btnAceptar
         '
@@ -127,7 +101,7 @@ Partial Class frm_CatalogoDeComercios
         Me.btnAceptar.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue
         Me.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btnAceptar.ForeColor = System.Drawing.SystemColors.Control
-        Me.btnAceptar.Location = New System.Drawing.Point(358, 167)
+        Me.btnAceptar.Location = New System.Drawing.Point(517, 167)
         Me.btnAceptar.Name = "btnAceptar"
         Me.btnAceptar.Size = New System.Drawing.Size(75, 23)
         Me.btnAceptar.TabIndex = 8
@@ -140,7 +114,7 @@ Partial Class frm_CatalogoDeComercios
         Me.btnCancelar.FlatAppearance.BorderColor = System.Drawing.Color.DimGray
         Me.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btnCancelar.ForeColor = System.Drawing.SystemColors.Control
-        Me.btnCancelar.Location = New System.Drawing.Point(265, 167)
+        Me.btnCancelar.Location = New System.Drawing.Point(436, 167)
         Me.btnCancelar.Name = "btnCancelar"
         Me.btnCancelar.Size = New System.Drawing.Size(75, 23)
         Me.btnCancelar.TabIndex = 9
@@ -150,7 +124,7 @@ Partial Class frm_CatalogoDeComercios
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(15, 172)
+        Me.Label2.Location = New System.Drawing.Point(222, 150)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(18, 13)
         Me.Label2.TabIndex = 10
@@ -159,7 +133,7 @@ Partial Class frm_CatalogoDeComercios
         '
         'txtID
         '
-        Me.txtID.Location = New System.Drawing.Point(39, 169)
+        Me.txtID.Location = New System.Drawing.Point(225, 165)
         Me.txtID.Name = "txtID"
         Me.txtID.ReadOnly = True
         Me.txtID.Size = New System.Drawing.Size(61, 20)
@@ -185,21 +159,21 @@ Partial Class frm_CatalogoDeComercios
         Me.Label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Label3.Location = New System.Drawing.Point(-63, -1)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(635, 64)
+        Me.Label3.Size = New System.Drawing.Size(669, 64)
         Me.Label3.TabIndex = 17
         '
         'frm_CatalogoDeComercios
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(442, 511)
+        Me.ClientSize = New System.Drawing.Size(604, 497)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.txtID)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.btnCancelar)
         Me.Controls.Add(Me.btnAceptar)
-        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.dGridComercios)
         Me.Controls.Add(Me.txtNombre)
         Me.Controls.Add(Me.lblNombre)
         Me.Controls.Add(Me.txtRazonSocial)
@@ -208,7 +182,7 @@ Partial Class frm_CatalogoDeComercios
         Me.Controls.Add(Me.lblRFC)
         Me.Name = "frm_CatalogoDeComercios"
         Me.Text = "Comercios autorizados"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dGridComercios, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -220,13 +194,9 @@ Partial Class frm_CatalogoDeComercios
     Friend WithEvents txtRazonSocial As TextBox
     Friend WithEvents lblNombre As Label
     Friend WithEvents txtNombre As TextBox
-    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents dGridComercios As DataGridView
     Friend WithEvents btnAceptar As Button
     Friend WithEvents btnCancelar As Button
-    Friend WithEvents IDComercio As DataGridViewTextBoxColumn
-    Friend WithEvents razonSocial As DataGridViewTextBoxColumn
-    Friend WithEvents nombreComercial As DataGridViewTextBoxColumn
-    Friend WithEvents rfc As DataGridViewTextBoxColumn
     Friend WithEvents Label2 As Label
     Friend WithEvents txtID As TextBox
     Friend WithEvents Label7 As Label
--- Prueba proyecto bdat/frm_CatalogoDeComercios.vb
+++ Prueba proyecto bdat/frm_CatalogoDeComercios.vb
@@ -1,7 +1,95 @@
 ﻿Public Class frm_CatalogoDeComercios
+    Dim actualizando = False
+    Dim puedoAgregar = False
     Private Sub frm_CatalogoDeComercios_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
         Me.Hide()
         frm_Menu.Show()
         frm_Menu.Focus()
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+    Private Sub LoadDataGrid()
+        'Me.dGridComercios.Rows.Clear()
+        'Me.dGridComercios.Columns.Clear()
+        Me.dGridComercios.DataSource = ConnectionModule.connection.ReaderCommand("SELECT idComercio, razonSocial, nombreComercial, RFC FROM Comercio WHERE activo = 1", "Comercio").Tables("Comercio")
+        Me.dGridComercios.Columns("idComercio").Visible = False
+        'Me.dGridComercios.Columns("IDComercio").DisplayIndex = 0
+        'Me.dGridComercios.Columns("razonSocial").DisplayIndex = 0
+        'Me.dGridComercios.Columns("nombreComercial").DisplayIndex = 1
+        'Me.dGridComercios.Columns("rfc").DisplayIndex = 2
+        'Me.dGridComercios.Columns("IDComercio").DisplayIndex = 3
+    End Sub
+    Private Sub frm_CatalogoDeComercios_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadDataGrid()
+    End Sub
+    Private Sub ResetControls()
+        txtID.Text = ""
+        txtNombre.Text = ""
+        txtRazonSocial.Text = ""
+        txtNombre.Text = ""
+        txtRFC.Text = ""
+    End Sub
+    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
+        ResetControls()
+        btnAceptar.Text = "Agregar"
+        actualizando = False
+        btnAceptar.BackColor = Color.FromArgb(0, 192, 0)
+    End Sub
+
+    Private Sub btnAceptar_Click(sender As Object, e As EventArgs) Handles btnAceptar.Click
+        If txtNombre.Text <> "" And txtRazonSocial.Text <> "" And (txtRFC.Text <> "") Then
+            'If (txtTelefono.Text.Length >= 8) Then
+            If actualizando <> True Then
+                ' If txtContraseña.Text <> "" Then
+                'If txtCorreo.Text.Contains("@") And txtCorreo.Text.Contains(".") Then
+                puedoAgregar = True
+                'Else
+                'MessageBox.Show("Ingrese un correo valido.", "Errror", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                'End If
+
+                'Else
+                'MessageBox.Show("Todos los campos son obligatorios.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                'End If
+            Else
+                    puedoAgregar = True
+                End If
+            'Else
+            'MessageBox.Show("El telefono debe ser de minimo 8 digitos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            'End If
+        Else
+            MessageBox.Show("Todos los campos son obligatorios.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+
+        If puedoAgregar = True Then
+            If txtID.Text = "" Then
+                ConnectionModule.connection.NonQueryCommand("INSERT INTO Comercio (razonSocial, nombreComercial, RFC) VALUES ('" & txtRazonSocial.Text & "','" & txtNombre.Text & "','" & txtRFC.Text & "')")
+            Else
+                ConnectionModule.connection.NonQueryCommand("UPDATE Comercio SET razonSocial = '" + txtRazonSocial.Text + "', nombreComercial = '" + txtNombre.Text + "', RFC = '" + txtRFC.Text + "' WHERE '" + txtID.Text + "' = idComercio")
+            End If
+
+            btnAceptar.Text = "Agregar"
+            actualizando = False
+            puedoAgregar = False
+            btnAceptar.BackColor = Color.FromArgb(0, 192, 0)
+            ConnectionModule.connection.Clear(dGridComercios)
+            ResetControls()
+            LoadDataGrid()
+            txtNombre.Focus()
+        End If
+    End Sub
+
+    Private Sub dGridComercios_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dGridComercios.CellContentClick
+        actualizando = True
+        If e.RowIndex < dGridComercios.RowCount - 1 Then
+            txtID.Text = dGridComercios.CurrentRow.Cells.Item(0).Value
+            txtRazonSocial.Text = dGridComercios.CurrentRow.Cells.Item(1).Value
+            txtNombre.Text = dGridComercios.CurrentRow.Cells.Item(2).Value
+            txtRFC.Text = dGridComercios.CurrentRow.Cells.Item(3).Value
+
+            btnAceptar.Text = "Actualizar"
+            btnAceptar.BackColor = Color.DodgerBlue
+        End If
     End Sub
 End Class
