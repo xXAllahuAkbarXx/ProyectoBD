d3f16d8430c92ecc5f3e6dbccc7fabd41ad56917
PanicJoker03 <buddynod32@hotmail.com>
2016-04-29 22:49:30 -0500

    commit que me pidio el visual

---
+++ Prueba proyecto bdat/Validacion.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Text.RegularExpressions
+
+
+Module Validation
+
+    Function OnlyLetters(ByRef value As String) As Boolean
+        Return Regex.IsMatch(value, "^[a-zA-Z]*$")
+    End Function
+
+End Module
