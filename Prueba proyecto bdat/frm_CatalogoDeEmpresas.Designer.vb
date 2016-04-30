24024c4b4f63f0e5fe12fdcbe667bcffcda6c2de
eloyedm <eloy.edm@gmail.com>
2016-04-30 02:51:27 -0500

    catalogo de empresas

--- Prueba proyecto bdat/frm_CatalogoDeEmpresas.Designer.vb
+++ Prueba proyecto bdat/frm_CatalogoDeEmpresas.Designer.vb
@@ -30,7 +30,7 @@ Partial Class frm_CatalogoDeEmpresas
         Me.txtNombre = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.txtID = New System.Windows.Forms.TextBox()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.dgrid_Empresa = New System.Windows.Forms.DataGridView()
         Me.idComercio = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.RCF = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.RazonSocial = New System.Windows.Forms.DataGridViewTextBoxColumn()
@@ -38,6 +38,8 @@ Partial Class frm_CatalogoDeEmpresas
         Me.dtpInicioDeOperaciones = New System.Windows.Forms.DateTimePicker()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.gboxDomicilio = New System.Windows.Forms.GroupBox()
+        Me.txtCp = New System.Windows.Forms.TextBox()
+        Me.CodigoP = New System.Windows.Forms.Label()
         Me.cbEstado = New System.Windows.Forms.ComboBox()
         Me.Label8 = New System.Windows.Forms.Label()
         Me.cbMunicipio = New System.Windows.Forms.ComboBox()
@@ -52,85 +54,95 @@ Partial Class frm_CatalogoDeEmpresas
         Me.btnCancelar = New System.Windows.Forms.Button()
         Me.Label9 = New System.Windows.Forms.Label()
         Me.Label10 = New System.Windows.Forms.Label()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        CType(Me.dgrid_Empresa, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.gboxDomicilio.SuspendLayout()
         Me.SuspendLayout()
         '
         'lblRFC
         '
         Me.lblRFC.AutoSize = True
-        Me.lblRFC.Location = New System.Drawing.Point(9, 75)
+        Me.lblRFC.Location = New System.Drawing.Point(12, 92)
+        Me.lblRFC.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblRFC.Name = "lblRFC"
-        Me.lblRFC.Size = New System.Drawing.Size(28, 13)
+        Me.lblRFC.Size = New System.Drawing.Size(35, 17)
         Me.lblRFC.TabIndex = 0
         Me.lblRFC.Text = "RFC"
         '
         'txtRFC
         '
-        Me.txtRFC.Location = New System.Drawing.Point(12, 91)
+        Me.txtRFC.Location = New System.Drawing.Point(16, 112)
+        Me.txtRFC.Margin = New System.Windows.Forms.Padding(4)
         Me.txtRFC.Name = "txtRFC"
-        Me.txtRFC.Size = New System.Drawing.Size(201, 20)
+        Me.txtRFC.Size = New System.Drawing.Size(267, 22)
         Me.txtRFC.TabIndex = 1
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(228, 75)
+        Me.Label1.Location = New System.Drawing.Point(304, 92)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(68, 13)
+        Me.Label1.Size = New System.Drawing.Size(89, 17)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Razon social"
         '
         'txtRazonSocial
         '
-        Me.txtRazonSocial.Location = New System.Drawing.Point(231, 91)
+        Me.txtRazonSocial.Location = New System.Drawing.Point(306, 112)
+        Me.txtRazonSocial.Margin = New System.Windows.Forms.Padding(4)
         Me.txtRazonSocial.Name = "txtRazonSocial"
-        Me.txtRazonSocial.Size = New System.Drawing.Size(201, 20)
+        Me.txtRazonSocial.Size = New System.Drawing.Size(267, 22)
         Me.txtRazonSocial.TabIndex = 2
         '
         'lblNombre
         '
         Me.lblNombre.AutoSize = True
-        Me.lblNombre.Location = New System.Drawing.Point(9, 123)
+        Me.lblNombre.Location = New System.Drawing.Point(12, 151)
+        Me.lblNombre.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblNombre.Name = "lblNombre"
-        Me.lblNombre.Size = New System.Drawing.Size(92, 13)
+        Me.lblNombre.Size = New System.Drawing.Size(122, 17)
         Me.lblNombre.TabIndex = 4
         Me.lblNombre.Text = "Nombre comercial"
         '
         'txtNombre
         '
-        Me.txtNombre.Location = New System.Drawing.Point(12, 139)
+        Me.txtNombre.Location = New System.Drawing.Point(16, 171)
+        Me.txtNombre.Margin = New System.Windows.Forms.Padding(4)
         Me.txtNombre.Name = "txtNombre"
-        Me.txtNombre.Size = New System.Drawing.Size(143, 20)
+        Me.txtNombre.Size = New System.Drawing.Size(189, 22)
         Me.txtNombre.TabIndex = 3
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(13, 284)
+        Me.Label2.Location = New System.Drawing.Point(19, 402)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(18, 13)
+        Me.Label2.Size = New System.Drawing.Size(21, 17)
         Me.Label2.TabIndex = 6
         Me.Label2.Text = "ID"
         Me.Label2.Visible = False
         '
         'txtID
         '
-        Me.txtID.Location = New System.Drawing.Point(37, 281)
+        Me.txtID.Location = New System.Drawing.Point(51, 398)
+        Me.txtID.Margin = New System.Windows.Forms.Padding(4)
         Me.txtID.Name = "txtID"
         Me.txtID.ReadOnly = True
-        Me.txtID.Size = New System.Drawing.Size(50, 20)
+        Me.txtID.Size = New System.Drawing.Size(65, 22)
         Me.txtID.TabIndex = 7
         Me.txtID.Visible = False
         '
-        'DataGridView1
+        'dgrid_Empresa
         '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.idComercio, Me.RCF, Me.RazonSocial, Me.Nombre})
-        Me.DataGridView1.Location = New System.Drawing.Point(12, 310)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(420, 221)
-        Me.DataGridView1.TabIndex = 8
+        Me.dgrid_Empresa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgrid_Empresa.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.idComercio, Me.RCF, Me.RazonSocial, Me.Nombre})
+        Me.dgrid_Empresa.Location = New System.Drawing.Point(17, 439)
+        Me.dgrid_Empresa.Margin = New System.Windows.Forms.Padding(4)
+        Me.dgrid_Empresa.Name = "dgrid_Empresa"
+        Me.dgrid_Empresa.Size = New System.Drawing.Size(560, 272)
+        Me.dgrid_Empresa.TabIndex = 8
         '
         'idComercio
         '
@@ -163,22 +175,29 @@ Partial Class frm_CatalogoDeEmpresas
         '
         'dtpInicioDeOperaciones
         '
-        Me.dtpInicioDeOperaciones.Location = New System.Drawing.Point(231, 139)
+        Me.dtpInicioDeOperaciones.CustomFormat = "yyyy-MM-dd"
+        Me.dtpInicioDeOperaciones.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.dtpInicioDeOperaciones.Location = New System.Drawing.Point(308, 171)
+        Me.dtpInicioDeOperaciones.Margin = New System.Windows.Forms.Padding(4)
         Me.dtpInicioDeOperaciones.Name = "dtpInicioDeOperaciones"
-        Me.dtpInicioDeOperaciones.Size = New System.Drawing.Size(200, 20)
+        Me.dtpInicioDeOperaciones.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.dtpInicioDeOperaciones.Size = New System.Drawing.Size(265, 22)
         Me.dtpInicioDeOperaciones.TabIndex = 4
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(228, 123)
+        Me.Label3.Location = New System.Drawing.Point(304, 151)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(108, 13)
+        Me.Label3.Size = New System.Drawing.Size(142, 17)
         Me.Label3.TabIndex = 10
         Me.Label3.Text = "Inicio de operaciones"
         '
         'gboxDomicilio
         '
+        Me.gboxDomicilio.Controls.Add(Me.txtCp)
+        Me.gboxDomicilio.Controls.Add(Me.CodigoP)
         Me.gboxDomicilio.Controls.Add(Me.cbEstado)
         Me.gboxDomicilio.Controls.Add(Me.Label8)
         Me.gboxDomicilio.Controls.Add(Me.cbMunicipio)
@@ -189,92 +208,120 @@ Partial Class frm_CatalogoDeEmpresas
         Me.gboxDomicilio.Controls.Add(Me.Label5)
         Me.gboxDomicilio.Controls.Add(Me.txtCalle)
         Me.gboxDomicilio.Controls.Add(Me.Label4)
-        Me.gboxDomicilio.Location = New System.Drawing.Point(13, 175)
+        Me.gboxDomicilio.Location = New System.Drawing.Point(17, 215)
+        Me.gboxDomicilio.Margin = New System.Windows.Forms.Padding(4)
         Me.gboxDomicilio.Name = "gboxDomicilio"
-        Me.gboxDomicilio.Size = New System.Drawing.Size(419, 100)
+        Me.gboxDomicilio.Padding = New System.Windows.Forms.Padding(4)
+        Me.gboxDomicilio.Size = New System.Drawing.Size(559, 164)
         Me.gboxDomicilio.TabIndex = 5
         Me.gboxDomicilio.TabStop = False
         Me.gboxDomicilio.Text = "Domicilio"
         '
+        'txtCp
+        '
+        Me.txtCp.Location = New System.Drawing.Point(75, 126)
+        Me.txtCp.Name = "txtCp"
+        Me.txtCp.Size = New System.Drawing.Size(100, 22)
+        Me.txtCp.TabIndex = 12
+        '
+        'CodigoP
+        '
+        Me.CodigoP.AutoSize = True
+        Me.CodigoP.Location = New System.Drawing.Point(18, 129)
+        Me.CodigoP.Name = "CodigoP"
+        Me.CodigoP.Size = New System.Drawing.Size(34, 17)
+        Me.CodigoP.TabIndex = 11
+        Me.CodigoP.Text = "C.P."
+        '
         'cbEstado
         '
         Me.cbEstado.FormattingEnabled = True
-        Me.cbEstado.Location = New System.Drawing.Point(56, 62)
+        Me.cbEstado.Location = New System.Drawing.Point(75, 76)
+        Me.cbEstado.Margin = New System.Windows.Forms.Padding(4)
         Me.cbEstado.Name = "cbEstado"
-        Me.cbEstado.Size = New System.Drawing.Size(144, 21)
+        Me.cbEstado.Size = New System.Drawing.Size(191, 24)
         Me.cbEstado.TabIndex = 4
         '
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(10, 65)
+        Me.Label8.Location = New System.Drawing.Point(15, 83)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(40, 13)
+        Me.Label8.Size = New System.Drawing.Size(52, 17)
         Me.Label8.TabIndex = 8
         Me.Label8.Text = "Estado"
         '
         'cbMunicipio
         '
         Me.cbMunicipio.FormattingEnabled = True
-        Me.cbMunicipio.Location = New System.Drawing.Point(276, 62)
+        Me.cbMunicipio.Location = New System.Drawing.Point(368, 79)
+        Me.cbMunicipio.Margin = New System.Windows.Forms.Padding(4)
         Me.cbMunicipio.Name = "cbMunicipio"
-        Me.cbMunicipio.Size = New System.Drawing.Size(136, 21)
+        Me.cbMunicipio.Size = New System.Drawing.Size(180, 24)
         Me.cbMunicipio.TabIndex = 5
         '
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(218, 65)
+        Me.Label7.Location = New System.Drawing.Point(291, 83)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(52, 13)
+        Me.Label7.Size = New System.Drawing.Size(67, 17)
         Me.Label7.TabIndex = 6
         Me.Label7.Text = "Municipio"
         '
         'txtColonia
         '
-        Me.txtColonia.Location = New System.Drawing.Point(312, 24)
+        Me.txtColonia.Location = New System.Drawing.Point(416, 30)
+        Me.txtColonia.Margin = New System.Windows.Forms.Padding(4)
         Me.txtColonia.Name = "txtColonia"
-        Me.txtColonia.Size = New System.Drawing.Size(100, 20)
+        Me.txtColonia.Size = New System.Drawing.Size(132, 22)
         Me.txtColonia.TabIndex = 3
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(264, 27)
+        Me.Label6.Location = New System.Drawing.Point(352, 33)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(42, 13)
+        Me.Label6.Size = New System.Drawing.Size(55, 17)
         Me.Label6.TabIndex = 4
         Me.Label6.Text = "Colonia"
         '
         'txtNumero
         '
-        Me.txtNumero.Location = New System.Drawing.Point(214, 24)
+        Me.txtNumero.Location = New System.Drawing.Point(285, 30)
+        Me.txtNumero.Margin = New System.Windows.Forms.Padding(4)
         Me.txtNumero.Name = "txtNumero"
-        Me.txtNumero.Size = New System.Drawing.Size(44, 20)
+        Me.txtNumero.Size = New System.Drawing.Size(57, 22)
         Me.txtNumero.TabIndex = 2
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(164, 27)
+        Me.Label5.Location = New System.Drawing.Point(219, 33)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(44, 13)
+        Me.Label5.Size = New System.Drawing.Size(58, 17)
         Me.Label5.TabIndex = 2
         Me.Label5.Text = "Número"
         '
         'txtCalle
         '
-        Me.txtCalle.Location = New System.Drawing.Point(46, 24)
+        Me.txtCalle.Location = New System.Drawing.Point(61, 30)
+        Me.txtCalle.Margin = New System.Windows.Forms.Padding(4)
         Me.txtCalle.Name = "txtCalle"
-        Me.txtCalle.Size = New System.Drawing.Size(112, 20)
+        Me.txtCalle.Size = New System.Drawing.Size(148, 22)
         Me.txtCalle.TabIndex = 1
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(10, 27)
+        Me.Label4.Location = New System.Drawing.Point(13, 33)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(30, 13)
+        Me.Label4.Size = New System.Drawing.Size(39, 17)
         Me.Label4.TabIndex = 0
         Me.Label4.Text = "Calle"
         '
@@ -284,9 +331,10 @@ Partial Class frm_CatalogoDeEmpresas
         Me.btnAgregar.FlatAppearance.BorderColor = System.Drawing.Color.RoyalBlue
         Me.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btnAgregar.ForeColor = System.Drawing.SystemColors.Control
-        Me.btnAgregar.Location = New System.Drawing.Point(357, 281)
+        Me.btnAgregar.Location = New System.Drawing.Point(478, 398)
+        Me.btnAgregar.Margin = New System.Windows.Forms.Padding(4)
         Me.btnAgregar.Name = "btnAgregar"
-        Me.btnAgregar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAgregar.Size = New System.Drawing.Size(100, 28)
         Me.btnAgregar.TabIndex = 6
         Me.btnAgregar.Text = "Agregar"
         Me.btnAgregar.UseVisualStyleBackColor = False
@@ -296,9 +344,10 @@ Partial Class frm_CatalogoDeEmpresas
         Me.btnCancelar.BackColor = System.Drawing.SystemColors.GrayText
         Me.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btnCancelar.ForeColor = System.Drawing.SystemColors.Control
-        Me.btnCancelar.Location = New System.Drawing.Point(261, 281)
+        Me.btnCancelar.Location = New System.Drawing.Point(350, 398)
+        Me.btnCancelar.Margin = New System.Windows.Forms.Padding(4)
         Me.btnCancelar.Name = "btnCancelar"
-        Me.btnCancelar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancelar.Size = New System.Drawing.Size(100, 28)
         Me.btnCancelar.TabIndex = 7
         Me.btnCancelar.Text = "Cancelar"
         Me.btnCancelar.UseVisualStyleBackColor = False
@@ -309,9 +358,10 @@ Partial Class frm_CatalogoDeEmpresas
         Me.Label9.BackColor = System.Drawing.Color.Maroon
         Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label9.ForeColor = System.Drawing.SystemColors.Control
-        Me.Label9.Location = New System.Drawing.Point(8, 19)
+        Me.Label9.Location = New System.Drawing.Point(11, 23)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(142, 25)
+        Me.Label9.Size = New System.Drawing.Size(176, 31)
         Me.Label9.TabIndex = 18
         Me.Label9.Text = "Crono-Vales"
         '
@@ -321,15 +371,16 @@ Partial Class frm_CatalogoDeEmpresas
         Me.Label10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.Label10.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Label10.Location = New System.Drawing.Point(0, 0)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(635, 64)
+        Me.Label10.Size = New System.Drawing.Size(846, 78)
         Me.Label10.TabIndex = 17
         '
         'frm_CatalogoDeEmpresas
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(453, 543)
+        Me.ClientSize = New System.Drawing.Size(604, 724)
         Me.Controls.Add(Me.Label9)
         Me.Controls.Add(Me.Label10)
         Me.Controls.Add(Me.btnCancelar)
@@ -337,7 +388,7 @@ Partial Class frm_CatalogoDeEmpresas
         Me.Controls.Add(Me.gboxDomicilio)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.dtpInicioDeOperaciones)
-        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.dgrid_Empresa)
         Me.Controls.Add(Me.txtID)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.txtNombre)
@@ -346,9 +397,10 @@ Partial Class frm_CatalogoDeEmpresas
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.txtRFC)
         Me.Controls.Add(Me.lblRFC)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "frm_CatalogoDeEmpresas"
         Me.Text = "Empresas"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgrid_Empresa, System.ComponentModel.ISupportInitialize).EndInit()
         Me.gboxDomicilio.ResumeLayout(False)
         Me.gboxDomicilio.PerformLayout()
         Me.ResumeLayout(False)
@@ -364,7 +416,7 @@ Partial Class frm_CatalogoDeEmpresas
     Friend WithEvents txtNombre As TextBox
     Friend WithEvents Label2 As Label
     Friend WithEvents txtID As TextBox
-    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents dgrid_Empresa As DataGridView
     Friend WithEvents idComercio As DataGridViewTextBoxColumn
     Friend WithEvents RCF As DataGridViewTextBoxColumn
     Friend WithEvents RazonSocial As DataGridViewTextBoxColumn
@@ -386,4 +438,7 @@ Partial Class frm_CatalogoDeEmpresas
     Friend WithEvents btnCancelar As Button
     Friend WithEvents Label9 As Label
     Friend WithEvents Label10 As Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents txtCp As System.Windows.Forms.TextBox
+    Friend WithEvents CodigoP As System.Windows.Forms.Label
 End Class
--- Prueba proyecto bdat/frm_CatalogoDeEmpresas.vb
+++ Prueba proyecto bdat/frm_CatalogoDeEmpresas.vb
@@ -1,7 +1,44 @@
 ﻿Public Class frm_CatalogoDeEmpresas
-    Private Sub frm_CatalogoDeEmpresas_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
-        Me.Hide()
-        frm_Menu.Show()
-        frm_Menu.Focus()
+    Dim actualizando = False
+    Dim puedoAgregar = False
+    Private Sub LoadDataGrid()
+        Me.dgrid_Empresa.DataSource = ConnectionModule.connection.ReaderCommand("SELECT idEmpresa, razonSocial, nombreComercial, RFC FROM Empresa WHERE activo = 1", "Empresa").Tables("Empresa")
+    End Sub
+
+    Private Sub ResetControls()
+        txtID.Text = ""
+        txtNombre.Text = ""
+        txtRFC.Text = ""
+        txtRazonSocial.Text = ""
+        dtpInicioDeOperaciones.Text = ""
+        txtCalle.Text = ""
+        txtColonia.Text = ""
+        cbEstado.Text = ""
+        cbMunicipio.Text = ""
+
+    End Sub
+
+    Private Sub frm_CatalogoDeEmpresas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadDataGrid()
+        cbEstado.DataSource = ConnectionModule.connection.ReaderCommand("SELECT idEstado, nombreEstado FROM Estado", "Estado").Tables("Estado")
+        cbEstado.DisplayMember = "nombreEstado"
+        cbEstado.ValueMember = "idEstado"
+
+    End Sub
+
+   
+
+  
+
+    Private Sub cbEstado_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbEstado.SelectedIndexChanged
+        Dim seleccion = cbMunicipio.SelectedIndex
+        cbMunicipio.DataSource = ConnectionModule.connection.ReaderCommand("SELECT idCiudad, nombreCiudad FROM Ciudad WHERE idEstado = " & cbEstado.SelectedIndex & "", "Ciudad").Tables("Ciudad")
+        cbMunicipio.DisplayMember = "nombreCiudad"
+        cbMunicipio.ValueMember = "idCiudad"
+    End Sub
+
+   
+    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
+        ConnectionModule.connection.NonQueryCommand("INSERT INTO Empresa (RFC,razonSocial,nombreComercial,fechaInicioOperaciones,calle,numero,colonia,idCiudad,idEstado,idPais,codigoPostal,activo) VALUES ('" & txtRFC.Text & "','" & txtRazonSocial.Text & "','" & txtNombre.Text & "','" & dtpInicioDeOperaciones.Text & "', '" & txtCalle.Text & "', '" & txtNumero.Text & "', '" & txtColonia.Text & "', '" & cbMunicipio.SelectedIndex & "', '" & cbEstado.SelectedIndex & "', '1', '" & txtCp.Text & "', '1')")
     End Sub
 End Class
